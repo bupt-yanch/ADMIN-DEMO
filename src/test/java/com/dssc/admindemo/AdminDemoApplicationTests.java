608b76d2065656ca5ab3744c790f17d457e93518
yanch <bupt-yanch@users.noreply.github.com>
2021-05-20 21:34:30 +0800

    first init

---
+++ src/test/java/com/dssc/admindemo/AdminDemoApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.dssc.admindemo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AdminDemoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
